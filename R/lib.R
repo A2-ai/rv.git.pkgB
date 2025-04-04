73f78cd5bd2cf5cdb75740791c7d7d936d815bf4
wes-a2ai <wes@a2-ai.com>
2025-04-04 15:32:34 +0000

    actual version 3

--- R/lib.R
+++ R/lib.R
@@ -2,6 +2,6 @@
 #' @importFrom rv.git.pkgA what_version_am_i
 
 what_version_am_i <- function() {
-  print("pkgB - v2")
+  print("pkgB - v3")
   print(sprintf("dependency %s", rv.git.pkgA::what_version_am_i()))
 }
