25911fad3547969c0c200bdf2266d265e3828363
wes-a2ai <wes@a2-ai.com>
2025-04-04 15:41:13 +0000

    version 5

--- R/lib.R
+++ R/lib.R
@@ -2,5 +2,5 @@
 #' @importFrom rv.git.pkgA what_version_am_i
 
 what_version_am_i <- function() {
-  sprintf("pkgB - v4\ndependencies: %s", rv.git.pkgA::what_version_am_i())
+  sprintf("pkgB - v5\ndependencies: %s", rv.git.pkgA::what_version_am_i())
 }
